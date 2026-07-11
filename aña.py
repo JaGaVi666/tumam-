2a844be770a05c52cd247ed01d3ecbd138a463bd
JaGaVi666 <ja.gangas@duocuc.cl>
2026-07-11 18:23:46 -0400

    Donas

---
+++ aña.py
@@ -0,0 +1,10 @@
+import random
+
+donas = {
+    #Nombre
+    "Saturatron" : {"Masa": "chocolate", "Glaseado": "chocolate", "Chispas": "chocolate", "Forma": "sin hueco"},
+    "Diabetron" : {"Masa": "Vainilla", "Glaseado": "crema", "Chispas": "colores", "Forma": "con hueco"},
+    "Unigordio" : {"Masa": "Vainilla", "Glaseado": "frutilla", "Chispas": "colores", "Forma": "con hueco.", "Adorno": "cuerno dulce"}
+}
+
+
