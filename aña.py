12eda712fd73c5c5e04173e09f9a2dff1cdcc16c
JaGaVi666 <ja.gangas@duocuc.cl>
2026-07-12 10:08:27 -0400

    leer pedido

--- aña.py
+++ aña.py
@@ -15,3 +15,46 @@ match opc:
         print(donas["Diabetron"])
     case 3:
         print(donas["Unigordio"])
+a = len(donas) - 1 
+for dona in donas:
+    print(dona, donas[dona])
+
+def crear():
+    print("Creando pedido. . .")
+    primer = random.randint(1,3)
+    a = random.randint (1,10)
+    b = 0
+    c = 0
+    if primer > 1:
+        b = random.randint (1,10)
+    if primer > 2:
+        c = random.randint (1,10)
+    pedidos = [a, b, c]
+    return pedidos
+
+def leerpedido(pedido):
+    vuelta = 0
+    print("El cliente pidió: ")
+    for dona in donas:
+        if pedido[vuelta] > 0:
+            vuelta = vuelta + 1
+            print(pedido[vuelta], " donas de ", dona)
+
+
+
+while True:
+    try:
+        opcion = int(input("¿Qué harás?: "))
+    except:
+        print("Solo interactuar con números.")
+    else:
+        match opcion:
+            case 1:
+                pedido = crear()
+
+
+
+
+
+
+
